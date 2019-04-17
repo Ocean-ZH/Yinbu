d73d1e70fe917f3c6e2d5e96cfc84e68bee44bd6
Jack Feng <fdy409784@outlook.com>
2019-04-17 17:09:23 +0800

    Revert "增加留言模块"
    This reverts commit 2a3e01be187fd5b79456ca4921b694c99de0d956.

---
+++ v-web/dist/js/chunk-2d210c47.88e62334.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d210c47"],{b8fa:function(n,t,c){"use strict";c.r(t);var e=function(){var n=this,t=n.$createElement,c=n._self._c||t;return c("div",[n._v("\n    Contact\n")])},a=[],u=(c("cadf"),c("551c"),c("f751"),c("097d"),{data:function(){return{}}}),r=u,d=c("2877"),f=Object(d["a"])(r,e,a,!1,null,"4d07f786",null);t["default"]=f.exports}}]);
+//# sourceMappingURL=chunk-2d210c47.88e62334.js.map
